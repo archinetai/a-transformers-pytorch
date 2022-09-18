feat: add correct monolith masking

a_transformers_pytorch/monolith.py
@@ -5,7 +5,7 @@ import torch
 from einops import rearrange
 from torch import Tensor, nn
 
-from .transformers import Resampler, Transformer, TransformerRotator, exists
+from .transformers import Resampler, Transformer, TransformerShifter, exists
 
 
 class MonolithBase(nn.Module):
@@ -40,7 +40,7 @@ class MonolithBase(nn.Module):
 
         self.transformers = nn.ModuleList(
             [
-                TransformerRotator(
+                TransformerShifter(
                     Transformer(
                         features=features,
                         max_length=layer_tokens[i][0],
@@ -48,18 +48,18 @@ class MonolithBase(nn.Module):
                         head_features=head_features,
                         num_heads=num_heads,
                         multiplier=multiplier,
+                        causal=True,
                         use_cross_attention=True,
                     ),
-                    num_rotations=1,
+                    num_shift=1,
                 )
                 for i in range(self.num_layers)
             ]
         )
 
     def forward(self, embedding: Tensor) -> Tensor:
-        batch = embedding.shape[0]
-        layers: List[Tensor] = [embedding]
-        context, input_embedding = None, embedding
+        batch, device = embedding.shape[0], embedding.device
+        layers, context, input_embedding = [embedding], None, embedding
 
         for i in range(self.num_layers - 1):
             # Compute compressed tokens bottom-up
@@ -70,15 +70,38 @@ class MonolithBase(nn.Module):
 
         for i in reversed(range(self.num_layers)):
             # Compute next token autoregressively top-down
-            in_tokens, out_tokens = self.layer_tokens[i]
-            embedding = layers.pop()[:, -in_tokens:] if layers else None  # type: ignore
-            context = context[:, -out_tokens:] if exists(context) else embedding  # type: ignore
-            context = self.transformers[i](embedding, context=context)
+            embedding_length, context_length = self.layer_tokens[i]
+            hierarchy_in, hierarchy_out = self.hierarchy
+            embedding = layers.pop()[:, -embedding_length:] if layers else None  # type: ignore
+            context = context[:, -context_length:] if exists(context) else embedding  # type: ignore
+            context = self.transformers[i](
+                embedding,
+                context=context,
+                cross_attention_mask=self.get_mask(i, device=device),
+            )
 
+        # Shift input embedding once and concatenate predicted output embedding
         output_head = input_embedding[:, 1 : -self.layer_tokens[0][0] + 1]
         output_tail = context
         output_embedding = torch.cat([output_head, output_tail], dim=-2)  # type: ignore
         return output_embedding
+
+    def get_mask(self, layer: int, device: torch.device) -> Tensor:
+        n, m, kwargs = *self.layer_tokens[layer], dict(dtype=torch.bool, device=device)
+
+        if layer == self.num_layers - 1:  # Causal mask if top layer
+            return ~torch.ones((n, m), **kwargs).triu(1)  # type: ignore
+
+        # Makes sure current level tokens cannot see future by using tokens from top
+        mask = torch.zeros((n, m), **kwargs)  # type: ignore
+        mask[-1, :] = True  # Only new token as access to last context token
+        n1, m1, hi, ho = n - 1, m - 1, *self.hierarchy[layer]
+        while n1 >= 0 and m1 >= 0:
+            n0, m0 = max(n1 - hi, 0), 0
+            shape = (n1 - n0, m1 - m0)
+            mask[n0:n1, m0:m1] = torch.ones(shape, **kwargs)  # type: ignore
+            n1, m1 = n1 - hi, m1 - ho
+        return mask
 
     def get_info(self):
         factors = [hi / ho for (hi, ho) in self.hierarchy]
@@ -87,69 +110,28 @@ class MonolithBase(nn.Module):
         return info
 
 
-class Monolith(nn.Module):
-    def __init__(
-        self,
-        num_tokens: int,
-        features: int,
-        head_features: int,
-        num_heads: int,
-        multiplier: int,
-        hierarchy: List[Tuple[int, int]],
-        layer_tokens: List[Tuple[int, int]],
-    ):
-        super().__init__()
-        self.hierarchy = hierarchy
-        self.layer_tokens = layer_tokens
-        self.num_layers = len(layer_tokens)
+# class Monolith(nn.Module):
+#     def __init__(self, monolith: MonolithBase, num_tokens: int):
+#         super().__init__()
+#         self.monolith = monolith
+#         self.features = momolith.features
+#         self.num_tokens = num_tokens
 
-        self.to_embedding = nn.Embedding(num_tokens, features)
-        self.token = nn.Parameter(torch.randn(features))
+#         self.to_embedding = nn.Embedding(num_tokens, features)
+#         self.token = nn.Parameter(torch.randn(features))
+#         self.to_logits = nn.Linear(in_features=features, out_features=num_tokens)
 
-        self.resamplers = nn.ModuleList(
-            [
-                Resampler(
-                    features=features,
-                    in_tokens=hierarchy[i][0],
-                    out_tokens=hierarchy[i][1],
-                    num_layers=4,
-                    head_features=head_features,
-                    num_heads=num_heads,
-                    multiplier=multiplier,
-                )
-                for i in range(self.num_layers - 1)
-            ]
-        )
+#     def forward(self, tokens: Tensor, return_logits: bool = False) -> Tensor:
+#         input_tokens = tokens if return_logits else tokens[:, :-1]
+#         embedding = self.to_embedding(input_tokens)
+#         output_embedding = self.compute_embedding(embedding)
 
-        self.transformers = nn.ModuleList(
-            [
-                Transformer(
-                    features=features,
-                    max_length=layer_tokens[i][0],
-                    num_layers=8,
-                    head_features=head_features,
-                    num_heads=num_heads,
-                    multiplier=multiplier,
-                    use_cross_attention=True,
-                )
-                for i in range(self.num_layers)
-            ]
-        )
+#         output_logits = self.to_logits(output_embedding)
+#         output_logits = rearrange(output_logits, "b n t -> b t n")
 
-        self.to_logits = nn.Linear(in_features=features, out_features=num_tokens)
-
-    def forward(self, tokens: Tensor, return_logits: bool = False) -> Tensor:
-        pass
-        # input_tokens = tokens if return_logits else tokens[:, :-1]
-        # embedding = self.to_embedding(input_tokens)
-        # output_embedding = self.compute_embedding(embedding)
-
-        # output_logits = self.to_logits(output_embedding)
-        # output_logits = rearrange(output_logits, "b n t -> b t n")
-
-        # if return_logits:
-        #     return output_logits
-        # # Compute and return loss
-        # target_tokens = tokens[:, -self.layer_tokens[0][0] :]
-        # loss = F.cross_entropy(output_logits, target_tokens)
-        # return loss
+#         if return_logits:
+#             return output_logits
+#         # Compute and return loss
+#         target_tokens = tokens[:, -self.layer_tokens[0][0] :]
+#         loss = F.cross_entropy(output_logits, target_tokens)
+#         return loss

a_transformers_pytorch/transformers.py
@@ -50,7 +50,11 @@ Attention Components
 
 
 def add_mask(sim: Tensor, mask: Tensor) -> Tensor:
-    mask = rearrange(mask, "b n m -> b 1 n m")
+    b, ndim = sim.shape[0], mask.ndim
+    if ndim == 3:
+        mask = rearrange(mask, "b n m -> b 1 n m")
+    if ndim == 2:
+        mask = repeat(mask, "n m -> b 1 n m", b=b)
     max_neg_value = -torch.finfo(sim.dtype).max
     sim = sim.masked_fill(~mask, max_neg_value)
     return sim
@@ -234,9 +238,6 @@ class TransformerBlock(nn.Module):
             )
 
         if use_cross_attention:
-            assert_message = "context_features required if use_cross_attention=True"
-            assert exists(context_features), assert_message
-
             self.cross_attention = Attention(
                 features=features,
                 head_features=head_features,
@@ -299,9 +300,12 @@ class Transformer(nn.Module):
         head_features: int,
         num_heads: int,
         multiplier: int,
-        use_positional_embedding: bool = True,
         causal: bool = False,
+        use_positional_embedding: bool = True,
+        use_attention: bool = True,
         use_cross_attention: bool = False,
+        context_features: Optional[int] = None,
+        out_features: Optional[int] = None,
     ):
         super().__init__()
         self.features = features
@@ -314,10 +318,13 @@ class Transformer(nn.Module):
                     head_features=head_features,
                     num_heads=num_heads,
                     multiplier=multiplier,
+                    causal=causal,
                     max_length=max_length,
                     use_positional_embedding=use_positional_embedding,
-                    causal=causal,
-                    context_features=features if use_cross_attention else None,
+                    use_attention=use_attention,
+                    use_cross_attention=use_cross_attention,
+                    context_features=context_features,
+                    out_features=out_features,
                 )
                 for i in range(num_layers)
             ]
@@ -329,16 +336,16 @@ class Transformer(nn.Module):
         return embedding
 
 
-class TransformerRotator(nn.Module):
-    def __init__(self, transformer: Transformer, *, num_rotations: int = 1):
+class TransformerShifter(nn.Module):
+    def __init__(self, transformer: Transformer, *, num_shift: int = 1):
         super().__init__()
         self.transformer = transformer
-        self.num_rotations = num_rotations
-        self.tokens = nn.Parameter(torch.randn(num_rotations, transformer.features))
+        self.num_shift = num_shift
+        self.tokens = nn.Parameter(torch.randn(num_shift, transformer.features))
 
     def forward(self, embedding: Tensor, **kwargs) -> Tensor:
         b = embedding.shape[0]
-        embedding_head = embedding[:, self.num_rotations :]
+        embedding_head = embedding[:, self.num_shift :]
         embedding_tail = repeat(self.tokens, "n d -> b n d", b=b)
         embedding = torch.cat([embedding_head, embedding_tail], dim=-2)
         embedding = self.transformer(embedding, **kwargs)