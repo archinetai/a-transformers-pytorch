566cb99f2253f3cf4be5ce16de8ab83bc897e324
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-10-03 22:27:50 +0200

    feat: use bias

--- a_transformers_pytorch/transformers.py
+++ a_transformers_pytorch/transformers.py
@@ -181,9 +181,9 @@ class Attention(nn.Module):
 def FeedForward(features: int, multiplier: int) -> nn.Module:
     mid_features = features * multiplier
     return nn.Sequential(
-        nn.Linear(in_features=features, out_features=mid_features, bias=False),
+        nn.Linear(in_features=features, out_features=mid_features),
         nn.GELU(),
-        nn.Linear(in_features=mid_features, out_features=features, bias=False),
+        nn.Linear(in_features=mid_features, out_features=features),
     )
 
 
